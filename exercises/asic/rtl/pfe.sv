74c03a81b2d33a96c7d2e20e7572fb2d109fe4ce
joovsns <trkulja.jovana@gimnazijaso.edu.rs>
2026-03-25 22:10:16 +0100

    Final v

--- exercises/asic/rtl/pfe.sv
+++ exercises/asic/rtl/pfe.sv
@@ -1,4 +1,233 @@
 module pfe #(
+    parameter int WIDTH = 16
+) (
+    input  logic             clk_i,
+    input  logic             rst_ni,
+    // Input
+    input  logic             in_valid_i,
+    output logic             in_ready_o,
+    // Output
+    output logic [WIDTH-1:0] out_data_o,
+    output logic             out_valid_o,
+    input  logic             out_ready_i,
+
+    input  logic [WIDTH-1:0] in_data_i,  //PERIOD
+    input  logic [WIDTH-1:0] duty_cycle,  // DUTY CYCLE
+    input  logic             but1,
+    input  logic             but2,
+    input  logic             but3,
+    input  logic             but4,
+    output logic             pwm_out
+);
+
+    assign in_ready_o = out_ready_i;
+    assign out_valid_o = in_valid_i;
+    assign out_data_o = '0;
+
+    logic [WIDTH-1:0] w_data;
+
+    always_ff @(posedge clk_i or negedge rst_ni) begin
+        if (!rst_ni)
+            w_data <= '0;
+        else if (in_valid_i && in_ready_o)
+            w_data <= in_data_i;
+    end
+
+  logic [WIDTH-1:0] w_count;
+  logic [WIDTH-1:0] w_duty;
+
+  counter #(
+      .WIDTH(WIDTH)
+  ) u_cnt (
+      .clk_i    (clk_i),
+      .period   (w_data), // w_data stavio bio Nikola
+      .counter_o(w_count)
+  );
+
+  buttons #(
+      .WIDTH(WIDTH)
+  ) u_btn (
+      .clk_i         (clk_i),
+      .period        (w_data), // w_data stavio bio Nikola
+        .duty_cycle    (duty_cycle),
+      .but1          (but1),
+      .but2          (but2),
+      .but3          (but3),
+      .but4          (but4),
+      .duty_cycle_out(w_duty)
+  );
+
+  pwm #(
+      .WIDTH(WIDTH)
+  ) u_pwm (
+      .clk_i     (clk_i),
+      .counter_i (w_count),
+      .duty_cycle(w_duty),
+      .value     (pwm_out)
+  );
+
+endmodule
+
+
+module counter #(
+    parameter int WIDTH = 16
+) (
+    input logic clk_i,
+
+    input logic [WIDTH-1:0] period,
+
+
+    output logic [WIDTH-1:0] counter_o
+);
+
+    initial counter_o = 0;
+  always_ff @(posedge clk_i) begin
+    if (counter_o >= period) counter_o <= 0;
+    else counter_o <= counter_o + 1;
+  end
+
+endmodule
+
+// Modified Buttons Module
+module buttons #(
+    parameter WIDTH = 16,
+    parameter DEBOUNCE_CNT_MAX = 20000  // Adjust as needed for debounce timing
+) (
+    input  logic             clk_i,
+    input  logic [WIDTH-1:0] period,
+    input  logic [WIDTH-1:0] duty_cycle,
+    input  logic             but1,
+    input  logic             but2,
+    input  logic             but3,
+    input  logic             but4,
+    output logic [WIDTH-1:0] duty_cycle_out
+);
+
+  logic b1_db, b2_db, b3_db, b4_db;
+  logic [31:0] cnt1, cnt2, cnt3, cnt4;
+
+  logic b1_r, b2_r, b3_r, b4_r;
+
+  initial begin
+    b1_db = 0; b2_db = 0; b3_db = 0; b4_db = 0;
+    b1_r  = 0; b2_r  = 0; b3_r  = 0; b4_r  = 0;
+    cnt1 = 0; cnt2 = 0; cnt3 = 0; cnt4 = 0;
+    duty_cycle_out = duty_cycle;
+  end
+
+  always_ff @(posedge clk_i) begin
+
+    // debounce
+    if (but1 == b1_db) cnt1 <= 0;
+    else begin
+      cnt1 <= cnt1 + 1;
+      if (cnt1 >= DEBOUNCE_CNT_MAX) begin
+        b1_db <= but1;
+        cnt1 <= 0;
+      end
+    end
+
+    if (but2 == b2_db) cnt2 <= 0;
+    else begin
+      cnt2 <= cnt2 + 1;
+      if (cnt2 >= DEBOUNCE_CNT_MAX) begin
+        b2_db <= but2;
+        cnt2 <= 0;
+      end
+    end
+
+    if (but3 == b3_db) cnt3 <= 0;
+    else begin
+      cnt3 <= cnt3 + 1;
+      if (cnt3 >= DEBOUNCE_CNT_MAX) begin
+        b3_db <= but3;
+        cnt3 <= 0;
+      end
+    end
+
+    if (but4 == b4_db) cnt4 <= 0;
+    else begin
+      cnt4 <= cnt4 + 1;
+      if (cnt4 >= DEBOUNCE_CNT_MAX) begin
+        b4_db <= but4;
+        cnt4 <= 0;
+      end
+    end
+
+    // edge detect
+    b1_r <= b1_db;
+    b2_r <= b2_db;
+    b3_r <= b3_db;
+    b4_r <= b4_db;
+
+    if (b1_db && !b1_r)
+      duty_cycle_out <= 0;
+    else if (b2_db && !b2_r)
+      duty_cycle_out <= duty_cycle_out + 1;
+    else if (b3_db && !b3_r)
+      duty_cycle_out <= duty_cycle_out + (period / 16);
+    else if (b4_db && !b4_r)
+      duty_cycle_out <= duty_cycle_out + (period / 4);
+
+    if (duty_cycle_out > period)
+      duty_cycle_out <= period;
+
+  end
+
+endmodule
+
+// pwm.sv (updated version)
+module pwm #(
+    parameter WIDTH = 16
+) (
+    input logic clk_i,
+    input logic [WIDTH-1:0] counter_i,
+    input logic [WIDTH-1:0] duty_cycle,
+    output logic value
+);
+
+    always_ff @(posedge clk_i) begin
+        // For 0% duty cycle, PWM should always be 0
+        if (duty_cycle == 0)
+            value <= 0;
+        // For 100% duty cycle, PWM should always be 1
+        else if (duty_cycle == 16'hFFFF)  // assuming max value for WIDTH = 16
+            value <= 1;
+        // For other duty cycles, toggle based on counter value
+        else if (counter_i <= duty_cycle)
+            value <= 1;
+        else
+            value <= 0;
+    end
+
+endmodule
+/*
+module pfe #(
+    parameter int WIDTH = 16
+)(
+    input  logic             clk_i,
+    input  logic             rst_ni,
+    // Input
+    input  logic [WIDTH-1:0] in_data_i,
+    input  logic             in_valid_i,
+    output logic             in_ready_o,
+    // Output
+    output logic [WIDTH-1:0] counter_o,
+    output logic             out_valid_o,
+    input  logic             out_ready_i
+);
+
+
+
+    assign in_ready_o = out_ready_i;
+    assign out_valid_o = in_valid_i;
+    assign counter_o = in_data_i;
+
+endmodule
+*/
+
+
+/*module pfe #(
     parameter int DSIZE  = 8
 )(
     input  logic             clk_i,
@@ -60,3 +289,4 @@ module pfe #(
     );
 
 endmodule
+*/
--- exercises/asic/rtl/pfe_chip.sv
+++ exercises/asic/rtl/pfe_chip.sv
@@ -1,128 +1,77 @@
 module pfe_chip (
-  input  wire clk_i,
-  input  wire rst_ni,
+    input  wire clk_i,
+    input  wire rst_ni,
 
-    // Input stream (producer -> FIFO)
-  input  wire in_valid_i,
-  output wire in_ready_o,
-  input  wire in_data_0_i,
-  input  wire in_data_1_i,
-  input  wire in_data_2_i,
-  input  wire in_data_3_i,
-  input  wire in_data_4_i,
-  input  wire in_data_5_i,
-  input  wire in_data_6_i,
-  input  wire in_data_7_i,
+    // Input stream (10 pads)
+    input  wire in_valid_i,
+    output wire in_ready_o,
+    input  wire in_data_0_i, in_data_1_i, in_data_2_i, in_data_3_i,
+    input  wire in_data_4_i, in_data_5_i, in_data_6_i, in_data_7_i,
 
-  // Output stream (FIFO -> consumer)
-  output wire out_valid_o,
-  input  wire out_ready_i,
-  output wire out_data_0_o,
-  output wire out_data_1_o,
-  output wire out_data_2_o,
-  output wire out_data_3_o,
-  output wire out_data_4_o,
-  output wire out_data_5_o,
-  output wire out_data_6_o,
-  output wire out_data_7_o,
+    // Output stream (6 pads)
+    output wire out_valid_o,
+    input  wire out_ready_i,
+    output wire out_data_0_o, out_data_1_o, out_data_2_o, out_data_3_o,
 
-  output wire unused0_o,
-  output wire unused1_o,
+    // User I/O (6 pads)
+    input  wire btn1_i, btn2_i, btn3_i, btn4_i,
+    output wire pwm_o,
+    output wire status_o,
 
-  inout wire VDD,
-  inout wire VSS,
-  inout wire VDDIO,
-  inout wire VSSIO
+    // Power (4 pads)
+    inout wire VDD, VSS, VDDIO, VSSIO
 ); 
-    logic soc_clk_i;
-    logic soc_rst_ni;
-    logic soc_ref_clk_i;
-    logic soc_testmode_i;
 
-    logic soc_jtag_tck_i;
-    logic soc_jtag_trst_ni;
-    logic soc_jtag_tms_i;
-    logic soc_jtag_tdi_i;
-    logic soc_jtag_tdo_o;
+    // Internal Signals
+    logic soc_clk_i, soc_rst_ni;
+    logic soc_in_valid, soc_in_ready;
+    logic [7:0] soc_in_data;
+    logic [3:0] soc_btns;
+    logic soc_pwm_out;
+    logic soc_out_valid, soc_out_ready;
+    logic [7:0] soc_out_data;
 
-    logic soc_status_o;
+    // --- PAD INSTANCES ---
+    sg13g2_IOPadIn pad_clk (.pad(clk_i), .p2c(soc_clk_i));
+    sg13g2_IOPadIn pad_rst (.pad(rst_ni), .p2c(soc_rst_ni));
 
-    localparam int unsigned DataCount = 16;
+    sg13g2_IOPadIn pad_in_valid (.pad(in_valid_i), .p2c(soc_in_valid));
+    sg13g2_IOPadOut16mA pad_in_ready (.pad(in_ready_o), .c2p(soc_in_ready));
 
-    logic                 soc_in_valid_i;
-    logic                 soc_in_ready_o;
-    logic [DataCount-1:0] soc_in_data_i; 
-    logic                 soc_out_valid_o;
-    logic                 soc_out_ready_i;            
-    logic [DataCount-1:0] soc_out_data_o;
+    sg13g2_IOPadIn pad_in_d0 (.pad(in_data_0_i), .p2c(soc_in_data[0]));
+    sg13g2_IOPadIn pad_in_d1 (.pad(in_data_1_i), .p2c(soc_in_data[1]));
+    sg13g2_IOPadIn pad_in_d2 (.pad(in_data_2_i), .p2c(soc_in_data[2]));
+    sg13g2_IOPadIn pad_in_d3 (.pad(in_data_3_i), .p2c(soc_in_data[3]));
+    sg13g2_IOPadIn pad_in_d4 (.pad(in_data_4_i), .p2c(soc_in_data[4]));
+    sg13g2_IOPadIn pad_in_d5 (.pad(in_data_5_i), .p2c(soc_in_data[5]));
+    sg13g2_IOPadIn pad_in_d6 (.pad(in_data_6_i), .p2c(soc_in_data[6]));
+    sg13g2_IOPadIn pad_in_d7 (.pad(in_data_7_i), .p2c(soc_in_data[7]));
 
-    sg13g2_IOPadIn        pad_clk_i        (.pad(clk_i),        .p2c(soc_clk_i));
-    sg13g2_IOPadIn        pad_rst_ni       (.pad(rst_ni),       .p2c(soc_rst_ni));
+    sg13g2_IOPadIn pad_btn1 (.pad(btn1_i), .p2c(soc_btns[0]));
+    sg13g2_IOPadIn pad_btn2 (.pad(btn2_i), .p2c(soc_btns[1]));
+    sg13g2_IOPadIn pad_btn3 (.pad(btn3_i), .p2c(soc_btns[2]));
+    sg13g2_IOPadIn pad_btn4 (.pad(btn4_i), .p2c(soc_btns[3]));
 
-    // in_data
-    sg13g2_IOPadIn        pad_in_valid_i    (.pad(in_valid_i),    .p2c(soc_in_valid_i));
-    sg13g2_IOPadOut16mA   pad_in_ready_o    (.pad(in_ready_o),    .c2p(soc_in_ready_o));
-    sg13g2_IOPadIn        pad_in_data_0_i   (.pad(in_data_0_i),   .p2c(soc_in_data_i[0]));
-    sg13g2_IOPadIn        pad_in_data_1_i   (.pad(in_data_1_i),   .p2c(soc_in_data_i[1]));
-    sg13g2_IOPadIn        pad_in_data_2_i   (.pad(in_data_2_i),   .p2c(soc_in_data_i[2]));
-    sg13g2_IOPadIn        pad_in_data_3_i   (.pad(in_data_3_i),   .p2c(soc_in_data_i[3]));
-    sg13g2_IOPadIn        pad_in_data_4_i   (.pad(in_data_4_i),   .p2c(soc_in_data_i[4]));
-    sg13g2_IOPadIn        pad_in_data_5_i   (.pad(in_data_5_i),   .p2c(soc_in_data_i[5]));
-    sg13g2_IOPadIn        pad_in_data_6_i   (.pad(in_data_6_i),   .p2c(soc_in_data_i[6]));
-    sg13g2_IOPadIn        pad_in_data_7_i   (.pad(in_data_7_i),   .p2c(soc_in_data_i[7]));
+    sg13g2_IOPadOut16mA pad_pwm    (.pad(pwm_o),    .c2p(soc_pwm_out));
+    sg13g2_IOPadOut16mA pad_status (.pad(status_o), .c2p(1'b1));
 
-    // out_data
-    sg13g2_IOPadOut16mA   pad_out_valid_o   (.pad(out_valid_o),   .c2p(soc_out_valid_o));
-    sg13g2_IOPadIn        pad_out_ready_i   (.pad(out_ready_i),   .p2c(soc_out_ready_i));
-    sg13g2_IOPadOut16mA   pad_out_data_0_o  (.pad(out_data_0_o),  .c2p(soc_out_data_o[0]));
-    sg13g2_IOPadOut16mA   pad_out_data_1_o  (.pad(out_data_1_o),  .c2p(soc_out_data_o[1]));
-    sg13g2_IOPadOut16mA   pad_out_data_2_o  (.pad(out_data_2_o),  .c2p(soc_out_data_o[2]));
-    sg13g2_IOPadOut16mA   pad_out_data_3_o  (.pad(out_data_3_o),  .c2p(soc_out_data_o[3]));
-    sg13g2_IOPadOut16mA   pad_out_data_4_o  (.pad(out_data_4_o),  .c2p(soc_out_data_o[4]));
-    sg13g2_IOPadOut16mA   pad_out_data_5_o  (.pad(out_data_5_o),  .c2p(soc_out_data_o[5]));
-    sg13g2_IOPadOut16mA   pad_out_data_6_o  (.pad(out_data_6_o),  .c2p(soc_out_data_o[6]));
-    sg13g2_IOPadOut16mA   pad_out_data_7_o  (.pad(out_data_7_o),  .c2p(soc_out_data_o[7]));
+    sg13g2_IOPadOut16mA pad_out_valid (.pad(out_valid_o), .c2p(soc_out_valid));
+    sg13g2_IOPadIn      pad_out_ready (.pad(out_ready_i), .p2c(soc_out_ready));
+    sg13g2_IOPadOut16mA pad_out_d0 (.pad(out_data_0_o), .c2p(soc_out_data[0]));
+    sg13g2_IOPadOut16mA pad_out_d1 (.pad(out_data_1_o), .c2p(soc_out_data[1]));
+    sg13g2_IOPadOut16mA pad_out_d2 (.pad(out_data_2_o), .c2p(soc_out_data[2]));
+    sg13g2_IOPadOut16mA pad_out_d3 (.pad(out_data_3_o), .c2p(soc_out_data[3]));
 
-    sg13g2_IOPadOut16mA pad_unused0_o      (.pad(unused0_o),    .c2p(soc_status_o));
-    sg13g2_IOPadOut16mA pad_unused1_o      (.pad(unused1_o),    .c2p(soc_status_o));
+    (* dont_touch = "true" *) sg13g2_IOPadVdd   pad_vdd0();
+    (* dont_touch = "true" *) sg13g2_IOPadVss   pad_vss0();
+    (* dont_touch = "true" *) sg13g2_IOPadIOVdd pad_vddio0();
+    (* dont_touch = "true" *) sg13g2_IOPadIOVss pad_vssio0();
 
-
-    (* dont_touch = "true" *)sg13g2_IOPadVdd pad_vdd0();
-    (* dont_touch = "true" *)sg13g2_IOPadVdd pad_vdd1();
-    (* dont_touch = "true" *)sg13g2_IOPadVdd pad_vdd2();
-    (* dont_touch = "true" *)sg13g2_IOPadVdd pad_vdd3();
-
-    (* dont_touch = "true" *)sg13g2_IOPadVss pad_vss0();
-    (* dont_touch = "true" *)sg13g2_IOPadVss pad_vss1();
-    (* dont_touch = "true" *)sg13g2_IOPadVss pad_vss2();
-    (* dont_touch = "true" *)sg13g2_IOPadVss pad_vss3();
-
-    (* dont_touch = "true" *)sg13g2_IOPadIOVdd pad_vddio0();
-    (* dont_touch = "true" *)sg13g2_IOPadIOVdd pad_vddio1();
-    (* dont_touch = "true" *)sg13g2_IOPadIOVdd pad_vddio2();
-    (* dont_touch = "true" *)sg13g2_IOPadIOVdd pad_vddio3();
-
-    (* dont_touch = "true" *)sg13g2_IOPadIOVss pad_vssio0();
-    (* dont_touch = "true" *)sg13g2_IOPadIOVss pad_vssio1();
-    (* dont_touch = "true" *)sg13g2_IOPadIOVss pad_vssio2();
-    (* dont_touch = "true" *)sg13g2_IOPadIOVss pad_vssio3();
-
-  pfe_soc #(
-    .DSIZE    (   8 ),
-    .ASIZE    (   8 ),
-    .USE_SRAM ( 1'b1 )
-  )
-  i_fifo_soc (
-    .clk_i          ( soc_clk_i       ),
-    .rst_ni         ( soc_rst_ni      ),
-    .in_valid_i     ( soc_in_valid_i  ),
-    .in_ready_o     ( soc_in_ready_o  ),
-    .in_data_i      ( soc_in_data_i   ),
-    .out_valid_o    ( soc_out_valid_o ),
-    .out_ready_i    ( soc_out_ready_i ),
-    .out_data_o     ( soc_out_data_o  )
-  );
-
-  assign soc_status_o = 1'b1;
-
+    // --- Core Logic ---
+    pfe #(.WIDTH(16)) i_core (
+        .clk_i(soc_clk_i), .rst_ni(soc_rst_ni),
+        .in_valid_i(soc_in_valid), .in_ready_o(soc_in_ready), .in_data_i(soc_in_data),
+        .duty_cycle(16'd0), .but1(soc_btns[0]), .but2(soc_btns[1]), .but3(soc_btns[2]), .but4(soc_btns[3]),
+        .pwm_out(soc_pwm_out), .out_valid_o(soc_out_valid), .out_ready_i(soc_out_ready), .out_data_o(soc_out_data)
+    );
 endmodule
